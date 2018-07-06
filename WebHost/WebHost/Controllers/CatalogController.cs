ae12544b1fc5692233fa5a510f532b222d0f1111
Baranov KA <baranovka@quarta.su>
2018-07-06 16:50:38 +0300

    Печи

---
+++ WebHost/WebHost/Controllers/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHost.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public ActionResult Furnaces()  // печи
+        {
+            return View();
+        }
+    }
+}
