9eda2fc08802e6f8336338b84a7c0dfa3b123f44
Baranov KA <baranovka@quarta.su>
2018-08-23 18:49:00 +0300

    Сауны

--- WebHost/WebHost/Controllers/CatalogController.cs
+++ WebHost/WebHost/Controllers/CatalogController.cs
@@ -18,6 +18,11 @@ namespace WebHost.Controllers
             return View();
         }
 
+        public ActionResult Sauna()
+        {
+            return View();
+        }
+
         public ActionResult IceGenerators()
         {
             return View();
