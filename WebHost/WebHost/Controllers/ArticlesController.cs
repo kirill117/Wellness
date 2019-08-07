d017d3617f5157afd9ab79a494b0e8d7304610a6
Kirill Baranov <kirill117@yandex.ru>
2019-08-07 20:28:25 +0300

    update

--- WebHost/WebHost/Controllers/ArticlesController.cs
+++ WebHost/WebHost/Controllers/ArticlesController.cs
@@ -13,5 +13,10 @@ namespace WebHost.Controllers
         {
             return View();
         }
+
+        public ActionResult Electric()
+        {
+            return View();
+        }
     }
 }
